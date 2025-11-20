c0ac6a1a0f778c6a52d226cdcdf4c2227378b64f
PrajwalGhadi <prajwal.ghadi28@gmail.com>
2025-11-20 19:18:22 +0530

    updated package.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,9 +26,10 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://192.168.1.13:5173', 'http://localhost:3000', 'https://full-stack-blog-app1.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://192.168.1.13:5173', 'http://localhost:3000', 'https://full-stack-blog-app1.vercel.app/', 'https://blogingapp1.netlify.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // If you're sending cookies/auth headers
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const authRouter = require('./routers/auth.route');
